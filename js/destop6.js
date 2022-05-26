3abaf8d8e2d77fa5b1c7d3a047f3d0d4535f7b03
ChienPhV <phvchien@gmail.com>
2022-05-26 18:27:54 +0700

    sửa destop6.js

--- js/destop6.js
+++ js/destop6.js
@@ -1,10 +1,16 @@
 const image_input=document.querySelector("#image_input");
+// const hide_button=document.getElementById("image_input")
+
 var uploaded_image="";
 image_input.addEventListener("change", function(){
+    
     const reader=new FileReader();
     reader.addEventListener("load", ()=>{
         uploaded_image=reader.result;
         document.querySelector("#display_image").style.backgroundImage=`url(${uploaded_image})`;
+        document.querySelector("#image_input").style.opacity=`0`;
     })
-    reader.readAsDataURL(this.files[0]);
+    reader.readAsDataURL(this.files[0]);  
+   
 })
+
